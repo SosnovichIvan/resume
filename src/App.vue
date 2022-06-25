7a1d3487755d1998fbce510dd9b5670a1865acd7
Ivan Sosnovich <isosnovich@yandex.ru>
2022-06-25 22:33:19 +0300

    Подключения Vue Разбитие на компоненты по логике Подключение шрифтов Настройка переменных цвета Установка иконки отображения во вкладке

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app">Hello</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+    .app {
+      background: var(--main_background);
+      width: 100vw;
+    }
+</style>
